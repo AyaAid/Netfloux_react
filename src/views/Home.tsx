788781f42476c9500a8efa95f4504446d65a2151
kilianrdgs <kilian.rdgs@free.fr>
2023-10-26 16:49:20 +0200

    page home design

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
 import '../assets/styles/Style.scss';
+import ShowList from '../components/filmlist/ShowList';
 
 function Home(){
     return(
         <>
         <Navbar/>
+        <ShowList/>
         </>
 
     );

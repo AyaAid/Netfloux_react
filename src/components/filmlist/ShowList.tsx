9d4ad966806ccf487f8a53103ddcd106da6f7fc5
celianloisel <celianloisel@gmail.com>
2023-10-27 02:02:19 +0200

    Add genre on home page

--- src/components/filmlist/ShowList.tsx
+++ src/components/filmlist/ShowList.tsx
@@ -1,34 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getShowsList } from "../../utils/api";
 import "./ShowList.scss";
 
-function ShowList() {
-  const [films, setFilms] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getShowsList("action", 1);
-      setFilms(data);
-    }
-    fetchData();
-  }, []);
+interface Film {
+    title: string;
+    images: {
+        backdrops: string;
+    };
+}
 
-  return (
-    <div className="film-block">
-      <h3>Action</h3>
-      <div className="film-carousel">
-        {films.map((film) => (
-          <div className="film">
-            <div className="film-image" key={film["title"]}>
-              {" "}
-              <div className="film-image-ban"><h4>{film["title"]}</h4></div>       
-              <img src={film["images"]["backdrops"]} alt="film"></img>
-          
+interface ShowListProps {
+    slug: string; // Update this to match your actual prop names
+    name: string; // Update this to match your actual prop names
+}
+
+function ShowList(props: ShowListProps) {
+    const [films, setFilms] = useState<Film[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getShowsList(props.slug, 1); // Use props.slug
+                setFilms(data);
+            } catch (error) {
+                console.error("Error fetching shows:", error);
+            }
+        }
+
+        fetchData();
+    }, [props.slug]); // Use props.slug
+
+    return (
+        <div className="film-block">
+            <h3>{props.name}</h3> {/* Use props.name */}
+            <div className="film-carousel">
+                {films.map((film) => (
+                    <div className="film" key={film.title}>
+                        <div className="film-image">
+                            <div className="film-image-ban">
+                                <h4>{film.title}</h4>
+                            </div>
+                            <img src={film.images.backdrops} alt="film" />
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default ShowList;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/navbar/Navbar';
 import '../assets/styles/Style.scss';
 import ShowList from '../components/filmlist/ShowList';
+import { getGenres } from '../utils/api';
 
-function Home(){
-    return(
+function Home() {
+    const [types, setTypes] = useState<{ name: string; slug: string; }[]>([]);
+
+    useEffect(() => {
+        getGenres()
+            .then(data => {
+                setTypes(data);
+            })
+            .catch(error => {
+                console.error('Error fetching genres:', error);
+            });
+    }, []);
+
+    // Define an array of types to exclude
+    const excludedTypes = [
+        'Biography',
+        'Holiday',
+        'Home And Garden',
+        'Mini Series',
+        'None',
+        'Short',
+        'Special Interest',
+        'Sporting Event',
+    ];
+
+    // Filter the types array to exclude certain types
+    const filteredTypes = types.filter(type => !excludedTypes.includes(type.name));
+
+    return (
         <>
-        <Navbar/>
-        <ShowList/>
+            <Navbar />
+            {filteredTypes.map((type, index) => (
+                <ShowList key={index} name={type.name} slug={type.slug} />
+            ))}
         </>
-
     );
 }
+
 export default Home;
